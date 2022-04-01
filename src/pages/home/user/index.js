290e46bdbdf3b929aa48b9b6b43379a01cc4bfbb
Keigo <morio211106@gmail.com>
2022-04-01 11:43:53 -0700

    2.1 add user

--- src/pages/home/user/index.js
+++ src/pages/home/user/index.js
@@ -4,22 +4,31 @@ import admin from "firebase-admin";
 import app from '../../../base';
 import Input from '../../../components/input';
 import { useAppContext } from '../../../contexts/AppContext';
-import { collection, addDoc } from "firebase/firestore"; 
+import { collection, query, where, onSnapshot, addDoc } from "firebase/firestore";
 
 
 const UsersPage = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-
-  const functions = require("firebase-functions");
   const context = useAppContext()
+  
+  const q = query(collection(context.db, "user"), where("state", "==", "CA"));
+  const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    const cities = [];
+    querySnapshot.forEach((doc) => {
+        cities.push(doc.data().name);
+    });
+    console.log("user ", cities.join(", "));
+  });
   const handleCreate = async () => {
-    console.log("KLILl")
     const docRef = await addDoc(collection(context.db, "user"), {
       email: "Tokyo",
-      displayName: "Japan"
+      displayName: "Japan",
+      password: "123456",
+      active: true,
     });
   }
+
   // Start listing users from the beginning, 1000 at a time.
 
   useEffect(() => {
